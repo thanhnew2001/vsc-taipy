0e93f15bc19f9ec730851764effdbff0f6f957a6
thanhnew2001 <thanh.hispvietnam@gmail.com>
2023-11-07 14:27:48 +0700

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,152 @@
+const vscode = require('vscode');
+const axios = require('axios');
+
+let disposable;
+let ghostTextDecorationType;
+let timer; // Timer to trigger the API call after a pause
+let lastCursorPosition; // Store the last cursor position for selected text
+
+// Maintain a list of active ghost text decorations
+let activeGhostTextDecorations = [];
+
+function activate(context) {
+    console.log('Ghost Text Extension is active!');
+
+    ghostTextDecorationType = vscode.window.createTextEditorDecorationType({
+        after: {
+            contentText: '', // Initialize contentText as empty
+            color: '#888888', // Ghost text color
+        },
+    });
+
+    disposable = vscode.window.onDidChangeTextEditorSelection(updateSelectedText);
+    vscode.workspace.onDidChangeTextDocument(onTextDocumentChange);
+
+    // Register the "insertGhostText" command to handle Tab key presses
+    context.subscriptions.push(vscode.commands.registerCommand('extension.insertGhostText', insertGhostText));
+}
+
+function updateSelectedText(event) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+
+    // Get the selected text before the cursor position
+    const selection = editor.selection;
+    lastCursorPosition = editor.selection.active;
+
+    if (!selection.isEmpty) {
+        // Trigger API call after a 3-second pause
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            triggerAPICall(editor);
+        }, 1500);
+    }
+}
+
+function onTextDocumentChange(event) {
+    // Handle Tab key presses separately when a document is modified
+    if (event.contentChanges[0]?.text === '\t') {
+        insertGhostText();
+    } 
+	else {
+        // Clear the active ghost text decorations
+        activeGhostTextDecorations = [];
+        // Update the editor's decorations to remove the ghost text
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            editor.setDecorations(ghostTextDecorationType, activeGhostTextDecorations);
+        }
+
+		//trigger it here
+		clearTimeout(timer);
+        timer = setTimeout(() => {
+            triggerAPICall(editor);
+        }, 500);
+    }
+}
+
+
+async function triggerAPICall(editor) {
+    // Get the selected text before the cursor position
+    const selectedText = editor.document.getText(new vscode.Range(new vscode.Position(0, 0), lastCursorPosition));
+
+	console.log("triggerAPICall->")+selectedText
+    if (selectedText) {
+        try {
+            // Send the selected text to an LLM API and get the response.
+            const ghostText = await sendTextToLLMAPI(selectedText);
+
+            // Create a new decoration with updated contentText.
+            const newDecoration = {
+                range: new vscode.Range(lastCursorPosition, editor.selection.active),
+                renderOptions: {
+                    after: {
+                        contentText: ghostText,
+                        color: '#888888',
+                    },
+                },
+            };
+
+            // Add the new decoration to the list of active decorations
+            activeGhostTextDecorations.push(newDecoration);
+
+            // Apply all active decorations
+            editor.setDecorations(ghostTextDecorationType, activeGhostTextDecorations);
+        } catch (error) {
+            console.error('Error fetching ghost text:', error);
+        }
+    }
+}
+
+async function sendTextToLLMAPI(text) {
+    // Define the API endpoint and request data.
+    const apiUrl = 'https://7b80-103-253-89-37.ngrok-free.app/api/generate';
+    const requestData = {
+        inputs: text,
+        parameters: {
+            max_new_tokens: 64,
+        },
+    };
+
+    // Send a POST request to the LLM API.
+    try {
+        const response = await axios.post(apiUrl, requestData);
+
+        if (response.status === 200) {
+            return response.data.generated_text;
+        } else {
+            throw new Error(`LLM API request failed with status code: ${response.status}`);
+        }
+    } catch (error) {
+        throw new Error(`Error sending text to LLM API: ${error.message}`);
+    }
+}
+
+function insertGhostText() {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) return;
+
+    // Check if there are active ghost text decorations
+    if (activeGhostTextDecorations.length > 0) {
+        const ghostText = activeGhostTextDecorations[0].renderOptions.after.contentText;
+
+        // Insert the ghost text at the cursor position
+        editor.edit(editBuilder => {
+            editBuilder.insert(lastCursorPosition, ghostText);
+        });
+
+        // Remove the active ghost text decoration
+        activeGhostTextDecorations = [];
+        editor.setDecorations(ghostTextDecorationType, activeGhostTextDecorations);
+    }
+}
+
+function deactivate() {
+    disposable.dispose();
+}
+
+module.exports = {
+    activate,
+    deactivate,
+};
